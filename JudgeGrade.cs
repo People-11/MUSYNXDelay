d2d981ca14fe6b9972bb6b8d46a53b2127f1ad67
People-11 <41558215+People-11@users.noreply.github.com>
2025-09-04 00:02:44 +0930

    Add Delay Window

--- JudgeGrade.cs
+++ JudgeGrade.cs
@@ -1,4 +1,10 @@
 using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using Microsoft.Win32.SafeHandles;
+using System.Diagnostics; 
 
 namespace BMSLib
 {
@@ -9,6 +15,10 @@ namespace BMSLib
 		public JudgeGrade()
 		{
 		}
+
+		private static long totalKnockDistance;
+		private static int totalJudgedNotes;
+		private static bool consoleInitialized;
 
 		public static int ToAccEx { get; private set; }
 
@@ -40,18 +50,22 @@ namespace BMSLib
 
 		public static void Reset()
 		{
-			JudgeGrade.baseAccuracy = 0f;
-			JudgeGrade.exAccuracy = 0f;
-			JudgeGrade.exactAccuracy = 0f;
-			JudgeGrade.greatAccuracy = 0f;
-			JudgeGrade.rightAccuracy = 0f;
-			JudgeGrade.TotalEx = 0;
-			JudgeGrade.TotalExact = 0;
-			JudgeGrade.TotalGreat = 0;
-			JudgeGrade.TotalRight = 0;
-			JudgeGrade.TotalMiss = 0;
-			JudgeGrade.TotalCombo = 0;
-			JudgeGrade.uploadScore = new float[6];
+			InitializeAndConfigureConsole();
+			baseAccuracy = 0f;
+			exAccuracy = 0f;
+			exactAccuracy = 0f;
+			greatAccuracy = 0f;
+			rightAccuracy = 0f;
+			TotalEx = 0;
+			TotalExact = 0;
+			TotalGreat = 0;
+			TotalRight = 0;
+			TotalMiss = 0;
+			TotalCombo = 0;
+			uploadScore = new float[6];
+			totalKnockDistance = 0L;
+			totalJudgedNotes = 0;
+			UpdateConsoleDisplay();
 		}
 
 		public static float GetJudgeGradeScore(long knockDistance)
@@ -87,47 +101,53 @@ namespace BMSLib
 
 		public static int GetJudgeGrade(long knockDistance)
 		{
+			if (Math.Abs(knockDistance) < 2500000L)
+			{
+				totalKnockDistance += knockDistance;
+				totalJudgedNotes++;
+			}
 			int result = -1;
 			bool flag = knockDistance < 0L;
-			knockDistance = Math.Abs(knockDistance);
-			if (knockDistance < 450000L)
+			long num = Math.Abs(knockDistance);
+			if (num < 450000L)
 			{
 				result = 0;
-				JudgeGrade.baseAccuracy += 1f;
-				JudgeGrade.exAccuracy += 0.05f + 0.1f * (1f - (float)knockDistance / 450000f);
-				JudgeGrade.exactAccuracy += 1f;
-				JudgeGrade.TotalEx++;
-				JudgeGrade.TotalExact++;
+				baseAccuracy += 1f;
+				exAccuracy += 0.05f + 0.1f * (1f - (float)num / 450000f);
+				exactAccuracy += 1f;
+				TotalEx++;
+				TotalExact++;
 			}
-			else if (knockDistance < 900000L)
+			else if (num < 900000L)
 			{
 				result = 1;
-				JudgeGrade.baseAccuracy += 1f;
-				JudgeGrade.exactAccuracy += 1f;
-				JudgeGrade.TotalExact++;
+				baseAccuracy += 1f;
+				exactAccuracy += 1f;
+				TotalExact++;
 			}
-			else if (knockDistance < 1500000L)
+			else if (num < 1500000L)
 			{
 				result = 2;
-				JudgeGrade.baseAccuracy += 0.7f + 0.2f * (1f - (float)(knockDistance - 900000L) / 600000f);
-				JudgeGrade.greatAccuracy += 0.7f + 0.2f * (1f - (float)(knockDistance - 900000L) / 600000f);
-				JudgeGrade.TotalGreat++;
+				baseAccuracy += 0.7f + 0.2f * (1f - (float)(num - 900000L) / 600000f);
+				greatAccuracy += 0.7f + 0.2f * (1f - (float)(num - 900000L) / 600000f);
+				TotalGreat++;
 			}
-			else if (knockDistance < 2500000L)
+			else if (num < 2500000L)
 			{
 				if (!flag)
 				{
 					result = 3;
-					JudgeGrade.baseAccuracy += 0.1f + 0.5f * (1f - (float)(knockDistance - 900000L) / 1600000f);
-					JudgeGrade.rightAccuracy += 0.1f + 0.5f * (1f - (float)(knockDistance - 900000L) / 1600000f);
-					JudgeGrade.TotalRight++;
+					baseAccuracy += 0.1f + 0.5f * (1f - (float)(num - 900000L) / 1600000f);
+					rightAccuracy += 0.1f + 0.5f * (1f - (float)(num - 900000L) / 1600000f);
+					TotalRight++;
 				}
 			}
 			else if (!flag)
 			{
 				result = 4;
-				JudgeGrade.TotalMiss++;
+				TotalMiss++;
 			}
+			UpdateConsoleDisplay();
 			return result;
 		}
 
@@ -155,38 +175,38 @@ namespace BMSLib
 
 		private static int GetRatacc()
 		{
-			int num = JudgeGrade.TotalExact + JudgeGrade.TotalGreat + JudgeGrade.TotalRight + JudgeGrade.TotalMiss;
+			int num = TotalExact + TotalGreat + TotalRight + TotalMiss;
 			if (num < 1)
 			{
 				return 0;
 			}
-			int num2 = (int)(JudgeGrade.baseAccuracy / (float)num * 10000f);
-			JudgeGrade.uploadScore[1] = JudgeGrade.baseAccuracy;
-			JudgeGrade.uploadScore[3] = (float)num;
+			int num2 = (int)(baseAccuracy / (float)num * 10000f);
+			uploadScore[1] = baseAccuracy;
+			uploadScore[3] = (float)num;
 			num2 = ((num2 <= 10000) ? num2 : 10000);
-			JudgeGrade.baseAccuracy = 0f;
+			baseAccuracy = 0f;
 			return num2;
 		}
 
 		private static int GetRatadd()
 		{
-			int num = JudgeGrade.TotalExact + JudgeGrade.TotalGreat + JudgeGrade.TotalRight + JudgeGrade.TotalMiss;
+			int num = TotalExact + TotalGreat + TotalRight + TotalMiss;
 			if (num < 1)
 			{
 				return 0;
 			}
-			int num2 = (int)(JudgeGrade.exAccuracy / (float)num * 100000f);
-			JudgeGrade.ToAccEx = (int)(JudgeGrade.exAccuracy / (float)num * 10000f);
-			JudgeGrade.ToAccExact = (int)(JudgeGrade.exactAccuracy / (float)num * 10000f);
-			JudgeGrade.ToAccGreat = (int)(JudgeGrade.greatAccuracy / (float)num * 10000f);
-			JudgeGrade.ToAccRight = (int)(JudgeGrade.rightAccuracy / (float)num * 10000f);
-			JudgeGrade.uploadScore[2] = JudgeGrade.exAccuracy;
-			JudgeGrade.uploadScore[3] = (float)num;
+			int num2 = (int)(exAccuracy / (float)num * 100000f);
+			ToAccEx = (int)(exAccuracy / (float)num * 10000f);
+			ToAccExact = (int)(exactAccuracy / (float)num * 10000f);
+			ToAccGreat = (int)(greatAccuracy / (float)num * 10000f);
+			ToAccRight = (int)(rightAccuracy / (float)num * 10000f);
+			uploadScore[2] = exAccuracy;
+			uploadScore[3] = (float)num;
 			num2 = ((num2 <= 15000) ? num2 : 15000);
-			JudgeGrade.exAccuracy = 0f;
-			JudgeGrade.exactAccuracy = 0f;
-			JudgeGrade.greatAccuracy = 0f;
-			JudgeGrade.rightAccuracy = 0f;
+			exAccuracy = 0f;
+			exactAccuracy = 0f;
+			greatAccuracy = 0f;
+			rightAccuracy = 0f;
 			return num2;
 		}
 
@@ -197,29 +217,35 @@ namespace BMSLib
 				return 0;
 			}
 			int num = (int)((float)maxCombo / (float)totalCombo * 10000f);
-			JudgeGrade.uploadScore[4] = (float)maxCombo;
-			JudgeGrade.uploadScore[5] = (float)totalCombo;
-			return (num <= 10000) ? num : 10000;
+			uploadScore[4] = (float)maxCombo;
+			uploadScore[5] = (float)totalCombo;
+			if (num > 10000)
+			{
+				return 10000;
+			}
+			return num;
 		}
 
 		public static void AddTotalCount(int addCombo)
 		{
-			JudgeGrade.TotalCombo += addCombo;
+			TotalCombo += addCombo;
 		}
 
 		public static int GetSyncNumber(int maxCombo)
 		{
 			DateTime now = DateTime.Now;
-			int num = JudgeGrade.GetRatacc() + JudgeGrade.GetRatadd() / 10 + JudgeGrade.GetRatseq(maxCombo, JudgeGrade.TotalCombo) / 10;
-			return (int)(JudgeGrade.GetUploadSyncNumber()[0] * 10000f) + num * 0;
+			GetRatacc();
+			int num = GetRatadd() / 10;
+			int num2 = GetRatseq(maxCombo, TotalCombo) / 10;
+			return (int)(GetUploadSyncNumber()[0] * 10000f);
 		}
 
 		public static float[] GetUploadSyncNumber()
 		{
-			JudgeGrade.uploadScore[0] = Math.Min(JudgeGrade.uploadScore[1] / JudgeGrade.uploadScore[3], 1f);
-			JudgeGrade.uploadScore[0] += Math.Min(JudgeGrade.uploadScore[2] / JudgeGrade.uploadScore[3], 0.15f);
-			JudgeGrade.uploadScore[0] += Math.Min(JudgeGrade.uploadScore[4] / JudgeGrade.uploadScore[5] / 10f, 0.1f);
-			return JudgeGrade.uploadScore;
+			uploadScore[0] = Math.Min(uploadScore[1] / uploadScore[3], 1f);
+			uploadScore[0] += Math.Min(uploadScore[2] / uploadScore[3], 0.15f);
+			uploadScore[0] += Math.Min(uploadScore[4] / uploadScore[5] / 10f, 0.1f);
+			return uploadScore;
 		}
 
 		public static string GetRankGeadeString(int synNumber)
@@ -269,5 +295,136 @@ namespace BMSLib
 		private static float[] uploadScore;
 
 		public static long ScriptDelay = 180000L;
+
+		private static void InitializeAndConfigureConsole()
+		{
+			if (consoleInitialized)
+			{
+				return;
+			}
+			NativeMethods.AllocConsole();
+			Thread.Sleep(50);
+			IntPtr consoleWindow = NativeMethods.GetConsoleWindow();
+			if (consoleWindow != IntPtr.Zero)
+			{
+
+				int style = NativeMethods.GetWindowLong(consoleWindow, NativeMethods.GWL_STYLE);
+				int newStyle = style & ~(NativeMethods.WS_CAPTION | NativeMethods.WS_THICKFRAME | NativeMethods.WS_SYSMENU | NativeMethods.WS_HSCROLL | NativeMethods.WS_VSCROLL);
+				NativeMethods.SetWindowLong(consoleWindow, NativeMethods.GWL_STYLE, newStyle);
+
+				uint flags = NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOACTIVATE;
+				NativeMethods.SetWindowPos(consoleWindow, NativeMethods.HWND_TOPMOST, 0, 0, 0, 0, flags);
+			}
+
+			IntPtr stdHandle = NativeMethods.GetStdHandle(-11);
+			SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, true);
+			FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+			StreamWriter writer = new StreamWriter(fileStream, Encoding.UTF8) { AutoFlush = true };
+			Console.SetOut(writer);
+
+			try
+			{
+				Console.SetWindowSize(30, 11);
+				Console.SetBufferSize(30, 11);
+			}
+			catch {}
+
+			IntPtr stdHandleFont = NativeMethods.GetStdHandle(-11);
+			NativeMethods.CONSOLE_FONT_INFOEX fontInfo = new NativeMethods.CONSOLE_FONT_INFOEX
+			{
+				cbSize = (uint)Marshal.SizeOf(typeof(NativeMethods.CONSOLE_FONT_INFOEX))
+			};
+			NativeMethods.GetCurrentConsoleFontEx(stdHandleFont, false, ref fontInfo);
+			fontInfo.dwFontSize.Y = 16;
+			fontInfo.FontWeight = 700;
+			fontInfo.FaceName = "Consolas";
+			NativeMethods.SetCurrentConsoleFontEx(stdHandleFont, false, ref fontInfo);
+			consoleInitialized = true;
+		}
+
+		private static void UpdateConsoleDisplay()
+		{
+			if (!consoleInitialized)
+			{
+				return;
+			}
+			Console.SetCursorPosition(0, 0);
+			float num = (totalJudgedNotes == 0) ? 0f : ((float)totalKnockDistance / (float)totalJudgedNotes / 10000f);
+			Console.Write("                            \n"); 
+			Console.Write("       -- Hit Stats --      \n");
+			Console.Write(" ---------------------------\n");
+			Console.Write(string.Format("  Ex Exact :  {0,-13}\n", TotalEx));
+			Console.Write(string.Format("  Exact    :  {0,-13}\n", TotalExact - TotalEx));
+			Console.Write(string.Format("  Great    :  {0,-13}\n", TotalGreat));
+			Console.Write(string.Format("  Right    :  {0,-13}\n", TotalRight));
+			Console.Write(string.Format("  Miss     :  {0,-13}\n", TotalMiss));
+			Console.Write(" ---------------------------\n");
+			Console.Write(string.Format("  Avg Delay:  {0,-8:F2} ms\n", num));
+		}
+
+		private static class NativeMethods
+		{
+
+			public const int GWL_STYLE = -16;
+			public const int WS_CAPTION = 0x00C00000;
+			public const int WS_THICKFRAME = 0x00040000;
+			public const int WS_SYSMENU = 0x00080000;
+			public const int WS_HSCROLL = 0x00100000;
+			public const int WS_VSCROLL = 0x00200000;
+			public static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+			public const uint SWP_NOMOVE = 0x0002;
+			public const uint SWP_NOSIZE = 0x0001;
+			public const uint SWP_NOACTIVATE = 0x0010;
+
+			[DllImport("kernel32.dll")]
+			public static extern bool AllocConsole();
+
+			[DllImport("kernel32.dll")]
+			public static extern IntPtr GetStdHandle(int nStdHandle);
+
+			[DllImport("kernel32.dll")]
+			public static extern IntPtr GetConsoleWindow();
+
+			[DllImport("user32.dll")]
+			public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+			[DllImport("user32.dll")]
+			public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+			[DllImport("user32.dll")]
+			public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+			[DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+			public static extern bool GetCurrentConsoleFontEx(IntPtr h, bool b, ref CONSOLE_FONT_INFOEX c);
+
+			[DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+			public static extern bool SetCurrentConsoleFontEx(IntPtr h, bool b, ref CONSOLE_FONT_INFOEX c);
+
+			[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+			public struct CONSOLE_FONT_INFOEX
+			{
+
+				public uint cbSize;
+
+				public uint nFont;
+
+				public COORD dwFontSize;
+
+				public int FontFamily;
+
+				public int FontWeight;
+
+				[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+				public string FaceName;
+			}
+
+			public struct COORD
+			{
+
+				public short X;
+
+				public short Y;
+			}
+		}
 	}
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+这是一个为 MUSYNX 添加实时延迟显示的 Patch  
+使用 dnSpy 打开 Assembly-CSharp.dll，定位到 BMSLib/JudgeGrade，然后替换为 repo 中的代码，保存模块后替换文件即可  
+窗口出现时会抢夺主窗口焦点，请注意  
+Inspired by: https://github.com/Ginsakura/MUSYNXSave
